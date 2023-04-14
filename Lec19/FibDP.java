b888a716571c20f633f8d51c054117e3b54b6e19
unknown <shubhamsinghal12@gmail.com>
2023-04-14 23:12:06 +0530

    Lecture 17

---
+++ Lec19/FibDP.java
@@ -0,0 +1,55 @@
+package Lec19;
+
+public class FibDP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n  =10000;
+//		System.out.println(fibTD(n, new int[n+1]));
+		System.out.println(fibBU(n));
+
+	}
+	
+	public static int fib(int n)
+	{
+		if(n == 1 || n == 0)
+			return n;
+		else
+		{
+			return fib(n-1)+fib(n-2);
+		}
+	}
+	
+	public static int fibTD(int n,int[] dp)
+	{
+		if(n == 0 || n == 1)
+		{
+			return n;
+		}
+		else if (dp[n] != 0)
+		{
+			return dp[n];
+		}
+		else
+		{
+			dp[n] = fibTD(n-1,dp)+fibTD(n-2,dp);
+			return dp[n];
+		}
+	}
+	
+	public static int fibBU(int n)
+	{
+		int[] dp = new int[n+1];
+		dp[0] = 0;
+		dp[1] = 1;
+		for(int i = 2; i < dp.length; i++)
+		{
+			dp[i] = dp[i-1]+ dp[i-2];
+		}
+		
+		return dp[n];
+	}
+	
+	
+
+}
